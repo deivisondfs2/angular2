16e08be02780141fb700dde090a11d6de450ab65
Deivison <deivisondfs@gmail.com>
2017-07-05 20:08:31 -0300

    remove files JS in app for new folder dist inside app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,19 +22,22 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>
-             <h2>{{hero.name}} details!!!!!</h2>
-             <div><label>id:</label>{{hero.id}}</div>
-             <div><label>name: </label>{{hero.name}}</div>
-             <div>
-                <label>name: </label>
-                <input [(ngModel)]="hero.name" placeholder="name">
+             <div *ngIf="selectedHero">
+              <h2>{{selectedHero.name}} details!</h2>
+              <div><label>id: </label>{{selectedHero.id}}</div>
+              <div>
+                  <label>name: </label>
+                  <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+              </div>
              </div>
              <h2>My Heroes</h2>
-              <ul class="heroes">
-                <li *ngFor= "let hero of heroes">
-                  <span class="badge">{{hero.id}}</span> {{hero.name}}
-                </li>
-              </ul>
+             <ul class="heroes">
+              <li *ngFor= "let hero of heroes" 
+                [class.selected]="hero === selectedHero"
+                (click) =  onSelect(hero)>
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+              </li>
+             </ul>
               `,
 })
 export class AppComponent  { 
@@ -47,7 +50,15 @@ export class AppComponent  {
   hero: Hero = {
         id: 1,
         name: 'Windstorm'
-        };
+      };
+      
+  
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+  }
 
 }
 
